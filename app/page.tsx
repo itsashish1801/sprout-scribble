873faba12feca66ddcc65f4517fb67830de404a8
itsashish1801 <itsashishsharma1801@gmail.com>
2024-09-03 19:37:55 +0530

    feat: Add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
-export default function Homepage() {
-  return <div>Homepage</div>;
+export default async function Homepage() {
+  return (
+    <main>
+      <h1>Homepage</h1>
+    </main>
+  );
 }
